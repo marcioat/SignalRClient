36287fb0b077b2799f299c28b33cc4cc4db09e8b
Marcio A teixeira <marcioat@magina.com.br>
2023-02-28 23:45:07 -0300

    Tutorial done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'SignalRClient';
+  private hubConnectionBuilder!: HubConnection;
+  offers: any[] = [];
+  constructor() {}
+  ngOnInit(): void {
+    this.hubConnectionBuilder = new HubConnectionBuilder()
+      .withUrl('http://localhost:5242/offers')
+      .configureLogging(LogLevel.Information)
+      .build();
+    this.hubConnectionBuilder
+      .start()
+      .then(() => console.log('Connection started.......!'))
+      .catch((err) => console.log('Error while connect with server'));
+    this.hubConnectionBuilder.on('SendOffersToUser', (result: any) => {
+      this.offers.push(result);
+    });
+  }
 }
